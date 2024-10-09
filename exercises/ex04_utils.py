a9528527e2a921d41c856b38ef22bb241536245b
Alyssa Schoof <schoof@unc.edu>
2024-10-09 18:34:34 -0400

    Finished ex04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -5,6 +5,8 @@ __author__ = "730674279"
 
 def all(a: list[int], b: int) -> bool:
     """Compares a given int to a list of ints; checks to see if they're the same"""
+    if a == []:
+        return False
     idx: int = 0
     while idx < len(a):
         if a[idx] == b:
@@ -26,18 +28,26 @@ def max(input: list[int]) -> int:
     """Returns the largest value in a list"""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
-    idx: int = 0
+    idx: int = 1
+    big_number: int = input[0]
     while idx < len(input):
-        if input[idx] > idx + 1:
-            print(input[idx])
+        if input[idx] > big_number:
+            big_number = input[idx]
         idx += 1
-    return idx
+    return big_number
+
+
+# Most of the code was written before going to tutoring
+# Had input[idx] set to equal big_number instead of the other way around
+# Was returning idx instead of big_number
 
 
 def is_equal(list_1: list[int], list_2: list[int]) -> bool:
     """Will determine if two lists are the same/equal to one another"""
+    if len(list_1) != len(list_2):
+        return False
     idx: int = 0
-    while idx < len(list_1 or list_2):
+    while idx < len(list_1):
         if list_1[idx] == list_2[idx]:
             idx += 1
         else:
@@ -46,8 +56,16 @@ def is_equal(list_1: list[int], list_2: list[int]) -> bool:
 
 
 # Used the code from the all function
-# Changed variables and set indicies equal to one another in line 41
+# Changed variables and set indicies equal to one another
+# Added if statement above idx to make the function return false
+# if presented with lists that were not equal
 
 
 def extend(list_1: list[int], list_2: list[int]) -> None:
     """Combines two lists into one longer list"""
+    for item in list_2:
+        list_1.append(item)
+
+
+# First attempt at for...in loop! Created "item" and then
+# appended it onto list_1
